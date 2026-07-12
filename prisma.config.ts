9d4e724b65fc9a423ac36a4fe2c27b83ff68b1b9
Akramjon001 <akramfarmonov998@gmail.com>
2026-07-12 12:58:46 +0500

    fix: Vercelda DATABASE_URL yoqligi sababli Prisma generate xatosini bartaraf etish

--- prisma.config.ts
+++ prisma.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy',
   },
 });
